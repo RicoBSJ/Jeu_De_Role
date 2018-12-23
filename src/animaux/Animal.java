a5b872c056af1ea9edb9b1182769e8950a339d05
RicoBSJ <aubrun.eric@free.fr>
2018-12-23 12:46:42 +0100

    Second Commit Home Animaux 23_12

---
+++ src/animaux/Animal.java
@@ -0,0 +1,5 @@
+package animaux;
+
+public abstract class Animal {
+	abstract void manger(); // Une méthode abstraite
+}
---
+++ src/animaux/Main_Animaux.java
@@ -0,0 +1,11 @@
+package animaux;
+
+public class Main_Animaux {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal ani = new Animal();
+		((Loup) ani).manger(); // Que doit-il faire ?
+	}
+
+}
