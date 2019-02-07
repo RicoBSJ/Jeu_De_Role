aea76c62ce5c855ddc4d1183ff05b73c92a7ca31
aubrune <aubrune@P520.ari.asso.fr>
2019-02-07 14:46:55 +0100

    5 Commit Job 07_02

---
+++ src/Personne.java
@@ -0,0 +1,22 @@
+
+public class Personne {
+
+	public Personne(int i, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getAge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object setAge(int i) {
+		// TODO Auto-generated method stub
+		return i;
+	}
+}
